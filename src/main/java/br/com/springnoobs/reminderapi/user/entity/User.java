14167467a25e7978e4a2e0e5ab42ecc1b0368e02
Lucas Barbosa Dourado <lucas.dourado@neomind.com.br>
2025-11-21 21:08:22 -0300

    feat: Criação da entidade User e Contact para guardas as informações de contato do usuário.

---
+++ src/main/java/br/com/springnoobs/reminderapi/user/entity/User.java
@@ -0,0 +1,60 @@
+package br.com.springnoobs.reminderapi.user.entity;
+
+import br.com.springnoobs.reminderapi.user.entity.contact.Contact;
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @OneToOne(mappedBy = "user")
+    private Contact contact;
+
+    public User() {}
+
+    public User(Long id, String firstName, String lastName, Contact contact) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.contact = contact;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Contact getContact() {
+        return contact;
+    }
+
+    public void setContact(Contact contact) {
+        this.contact = contact;
+    }
+}
---
+++ src/main/java/br/com/springnoobs/reminderapi/user/entity/contact/Contact.java
@@ -0,0 +1,60 @@
+package br.com.springnoobs.reminderapi.user.entity.contact;
+
+import br.com.springnoobs.reminderapi.user.entity.User;
+import jakarta.persistence.*;
+
+@Entity
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String email;
+
+    private String phoneNumber;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Contact() {}
+
+    public Contact(String email, String phoneNumber, User user) {
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}

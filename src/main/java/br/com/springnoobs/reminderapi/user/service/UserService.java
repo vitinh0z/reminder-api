769d0ae3f67d92a159ccb3ef1148ede8f780bf81
Joe <jonathan.colque@hotmail.com>
2025-11-26 12:14:24 -0300

    feat: Criação de testes unitarios para camada de serviço UserService.

--- src/main/java/br/com/springnoobs/reminderapi/user/service/UserService.java
+++ src/main/java/br/com/springnoobs/reminderapi/user/service/UserService.java
@@ -17,11 +17,11 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class UserService {
 
-    private final UserRepository userRepository;
+    private final UserRepository repository;
     private final ContactRepository contactRepository;
 
     public UserService(UserRepository userRepository, ContactRepository contactRepository) {
-        this.userRepository = userRepository;
+        this.repository = userRepository;
         this.contactRepository = contactRepository;
     }
 
@@ -34,7 +34,7 @@ public class UserService {
         user.setFirstName(dto.firstName());
         user.setLastName(dto.lastName());
 
-        User savedUser = userRepository.save(user);
+        User savedUser = repository.save(user);
 
         Contact contact = new Contact();
         contact.setEmail(dto.contactRequestDTO().email());
@@ -55,12 +55,12 @@ public class UserService {
     }
 
     public List<UserResponseDTO> findAll() {
-        List<User> users = userRepository.findAll();
+        List<User> users = repository.findAll();
         return UserMapper.toResponseList(users);
     }
 
     public void delete(Long id) {
-        User user = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("User not found" + id));
-        userRepository.delete(user);
+        User user = repository.findById(id).orElseThrow(() -> new UserNotFoundException("User not found" + id));
+        repository.delete(user);
     }
 }

f9d5b23b724a2908390f235eff07eaa2a4b15a25
Lucas Barbosa Dourado <douradoblucas@gmail.com>
2025-11-25 21:18:40 -0300

    feat: Adicionado H2 para testes em pipeline e perfil de testes utilizando H2 como banco de dados.

--- src/test/java/br/com/springnoobs/reminderapi/ReminderApiApplicationTests.java
+++ src/test/java/br/com/springnoobs/reminderapi/ReminderApiApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.springnoobs.reminderapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ReminderApiApplicationTests {
 
     @Test

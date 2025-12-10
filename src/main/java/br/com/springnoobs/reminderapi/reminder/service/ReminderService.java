3fbc8b8d57aba717c9694aad0281a7f293ff2f36
vitinh0z <victor10.vg23@gmail.com>
2025-12-10 18:14:24 -0300

    feat: Implementação do endpoint responsável por habilitar ou desabilitar o envio de e-mail

--- src/main/java/br/com/springnoobs/reminderapi/reminder/service/ReminderService.java
+++ src/main/java/br/com/springnoobs/reminderapi/reminder/service/ReminderService.java
@@ -13,7 +13,6 @@ import br.com.springnoobs.reminderapi.schedule.service.JobService;
 import br.com.springnoobs.reminderapi.user.entity.User;
 import br.com.springnoobs.reminderapi.user.service.UserService;
 import java.time.Instant;
-import java.util.Optional;
 
 import org.quartz.SchedulerException;
 import org.springframework.beans.BeanUtils;
@@ -118,27 +117,6 @@ public class ReminderService {
         repository.save(reminder);
     }
 
-    public ReminderResponseDTO disableEmail(Long id){
-
-        var reminder = repository.
-                findById(id)
-                .orElseThrow(() -> new NotFoundException("Reminder with ID: " + id + " not found")
-        );
-
-        reminder.setEmailEnabled(false);
-
-        return ReminderMapper.toResponse(repository.save(reminder));
-    }
-
-    public ReminderResponseDTO enableEmail(Long id){
-
-        var reminder = repository.
-                findById(id)
-                .orElseThrow(() -> new NotFoundException("Reminder with ID: " + id + " not found")
-        );
-
-        reminder.setEmailEnabled(true);
-        return ReminderMapper.toResponse(repository.save(reminder));
-    }
+    // Todo: Implementar a logica para desabilitar/habilitar o envio de email
 
 }
